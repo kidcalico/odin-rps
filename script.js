9d3395681c82e65bd1bde857b2f3bd4528585506
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-10-21 16:34:31 -0700

    Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+// console.log("Hello World!")
+
+// Write function getComputerChoice so that getComputerChoice will return "Rock", "Paper" or "Scissors"
+
+
+function getComputerChoice () {
+    const compChoice = Math.floor(Math.random() * 3) + 1;
+    if (compChoice === 1) {
+        return "Rock"
+    } else if (compChoice === 2 ) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice());
