f981f4e87f37b76142e119f82404def53af105ff
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-10-21 17:30:53 -0700

    Write the logic to play a single round

--- script.js
+++ script.js
@@ -14,16 +14,17 @@ function getComputerChoice () {
     }
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 // Write function getHumanChoice so that the user is prompted to enter either "Rock", "Paper" or "Scissors", and the function returns the choice
 
 function getHumanChoice() {
     let humChoice = prompt("Rock, Paper, or Scissors?", "Rock");
+    humChoice = humChoice.toLowerCase();
     return humChoice;
 }
 
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
 
 // Declare the players' score variables with an initial value of 0
 
@@ -32,7 +33,46 @@ let computerScore = 0;
 
 // Write the logic to play a single round
 // Declare a new function called playRound
-// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take both choices as arguments
-// Make humanChoice case-insensitive
 // Log a string value for playRound such as "You lose! Paper beats Rock!"
 // Increment the ___Score variable based on the winner
+
+function playRound () {
+    if (humanSelection === "rock" && computerSelection === "Rock") {
+        return "You both chose Rock! Try again!";
+    } else if (humanSelection === "rock" && computerSelection === "Paper") {
+        computerScore += 1;
+        return "The computer chose Paper, you lose. :(";
+    } else if (humanSelection === "rock" && computerSelection === "Scissors") {
+        humanScore += 1;
+        return "The computer chose Scissors, you win!!!";
+    } else if (humanSelection === "paper" && computerSelection === "Rock") {
+        humanScore += 1;
+        return "The computer chose Rock, you win!!!";
+    } else if (humanSelection === "paper" && computerSelection === "Paper") {
+        return "You both chose Paper! Try Again!";
+    } else if (humanSelection === "paper" && computerSelection === "Scissors") {
+        computerScore += 1;
+        return "The computer chose Scissors, you lose. :(";
+    } else if (humanSelection === "scissors" && computerSelection === "Rock") {
+        computerScore += 1;
+        return "The computer chose Rock, you lose. :(";
+    } else if (humanSelection === "scissors" && computerSelection === "Paper") {
+        humanScore += 1;
+        return "The computer chose Paper, you win!!!";
+    } else if (humanSelection === "scissors" && computerSelection === "Scissors") {
+        return "You both chose Scissors, try again!";
+    }
+}
+
+// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take both choices as arguments
+// Make humanChoice case-insensitive
+
+// getHumanChoice();
+// getComputerChoice();
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+console.log("Human Score: " + humanScore);
+console.log("Computer Score: " + computerScore);
