0a520ea409ce6ded6ef838790ab3ca817033c1c0
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-11-07 16:04:18 -0800

    Code ui for rock paper scissors
    So admittedly I haven't been adhering to the "commit early, commit
    often" rule, so here is what I have so far. There are still many bugs,
    but the buttons invoke the playRound function, which add to the correct
    score and announce the winner (or if it's a tie). I'm currently working
    on getting a "try again" button to pop up, and the game play to cease
    after either the human or computer reaches five points. For now the
    game will play infinitely. Also there is still a lot of extra code
    commented out, what can I say, sometimes I hoard. Bad habits, but I
    intend on correcting them. Baby steps. Also I intend on making more
    frequent commits on this project and others. That's all for now, byeee!

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // console.log("Hello World!")
+const interface = document.querySelector("#interface");
+interface.style.cssText = "color: blue; margin: auto; width; 500px;";
 
 // Write function getComputerChoice so that getComputerChoice will return "Rock", "Paper" or "Scissors"
 
@@ -19,12 +21,11 @@ function getComputerChoice() {
 // Write function getHumanChoice so that the user is prompted to enter either "Rock", "Paper" or "Scissors", and the function returns the choice
 // Make getHumanChoice case-insensitive
 
-function getHumanChoice() {
-    let humChoice = prompt("Rock, Paper, or Scissors?", "Rock");
-    humChoice = humChoice.toLowerCase();
-    return humChoice;
-}
-
+// function getHumanChoice() {
+//     let humChoice = prompt("Rock, Paper, or Scissors?", "Rock");
+//     humChoice = humChoice.toLowerCase();
+//     return humChoice;
+// }
 // console.log(getHumanChoice());
 
 // Declare the players' score variables with an initial value of 0
@@ -37,50 +38,103 @@ let computerScore = 0;
 // Log a string value for playRound such as "You lose! Paper beats Rock!"
 // Increment the ___Score variable based on the winner
 
-function playRound() {
-    if (humanSelection === "rock" && computerSelection === "Rock") {
-        return "You both chose Rock! Try again!";
-    } else if (humanSelection === "rock" && computerSelection === "Paper") {
-        computerScore += 1;
-        return "The computer chose Paper, you lose. :(";
-    } else if (humanSelection === "rock" && computerSelection === "Scissors") {
-        humanScore += 1;
-        return "The computer chose Scissors, you win!!!";
-    } else if (humanSelection === "paper" && computerSelection === "Rock") {
-        humanScore += 1;
-        return "The computer chose Rock, you win!!!";
-    } else if (humanSelection === "paper" && computerSelection === "Paper") {
-        return "You both chose Paper! Try Again!";
-    } else if (humanSelection === "paper" && computerSelection === "Scissors") {
-        computerScore += 1;
-        return "The computer chose Scissors, you lose. :(";
-    } else if (humanSelection === "scissors" && computerSelection === "Rock") {
-        computerScore += 1;
-        return "The computer chose Rock, you lose. :(";
-    } else if (humanSelection === "scissors" && computerSelection === "Paper") {
-        humanScore += 1;
-        return "The computer chose Paper, you win!!!";
-    } else if (humanSelection === "scissors" && computerSelection === "Scissors") {
-        return "You both chose Scissors, try again!";
+function playRound(humanSelection) {
+    if ((humanScore && computerScore) < 5) {
+        const computerSelection = getComputerChoice();
+        if ((humanSelection === "rock" && computerSelection === "Rock") ||
+        (humanSelection === "paper" && computerSelection === "Paper") ||
+        (humanSelection === "scissors" && computerSelection === "Scissors")) {
+            console.log(`You both chose ${computerSelection}! Try again!\nyou: ${humanScore} computer: ${computerScore}`);
+            announcer.textContent = `You both chose ${computerSelection}! Try again!`;
+        } else if ((humanSelection === "rock" && computerSelection === "Paper") ||
+        (humanSelection === "paper" && computerSelection === "Scissors") ||
+        (humanSelection === "scissors" && computerSelection === "Rock")) {
+            computerScore += 1;
+            if (computerScore < 5) {
+                console.log(`The computer chose ${computerSelection}, you lose. :(\nyou: ${humanScore} computer: ${computerScore}`);
+                announcer.textContent = `The computer chose ${computerSelection}, you lose. :(`;
+            } else {
+                announcer.textContent = "The computer wins, try again.";
+            }
+        } else {
+            humanScore += 1;
+            if (humanScore < 5) {
+                console.log(`The computer chose ${computerSelection}, you win!!!\nyou: ${humanScore} computer: ${computerScore}`);
+                announcer.textContent = `The computer chose ${computerSelection}, you win!!!`;
+            } else {
+                announcer.textContent = "You are the winner!!!";
+            }
+        }
+        scoreDiv.textContent = `You: ${humanScore} Computer: ${computerScore}`;
+    } else {
+        scoreBoard.appendChild(refBtn);
     }
 }
 
-function playGame() {
-    for(let i = 0; i < 5; i++) {
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-        // playRound(humanSelection, computerSelection);
-        console.log(playRound(humanSelection, computerSelection));
-        console.log("Human Score: " + humanScore);
-        console.log("Computer Score: " + computerScore);
-    }
-}
+// function playGame(humanSelection) {
+//     if ((humanScore && computerScore) < 5) {
+//         playRound(humanSelection);
+//     } else if (humanScore === 5) {
+//         announcer.textContent = "You are the winner!!!";
+//     } else {
+//         announcer.textContent = "The computer wins, try again.";
+//     }
+// }
 
 // Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take both choices as arguments
 
-let humanSelection;
-let computerSelection;
+// let humanSelection;
+// let computerSelection;
 
-// console.log(playRound(humanSelection, computerSelection));
+// for(let i = 0; i < 5; i++) {
+    //     // const humanSelection;
+    //     // playRound(humanSelection, computerSelection);
+    //     // console.log(playRound(humanSelection, computerSelection));
+    //     console.log("Human Score: " + humanScore);
+    //     console.log("Computer Score: " + computerScore);
+    // }
+    
+    
+    
+    // Create constants and add text to the buttons
+    
+    const rock = document.querySelector("#rock");
+    const paper = document.querySelector("#paper");
+    const scissors = document.querySelector("#scissors");
+    
+    rock.textContent = "Rock";
+    paper.textContent = "Paper";
+    scissors.textContent = "Scissors";
+    
+    // Create results section
+    
+    const scoreBoard = document.querySelector("#results");
+    scoreBoard.style.cssText = "padding: 20px; text-align: center; font-weight: bold";
+    scoreBoard.textContent = "Score";
+    
+    const scoreDiv = document.createElement("div");
+    scoreBoard.appendChild(scoreDiv);
+    scoreDiv.textContent = `You: ${humanScore} Computer: ${computerScore}`;
 
-console.log(playGame());
+    const announcer = document.createElement("div");
+    scoreBoard.appendChild(announcer);
+
+    // Create button that refreshes the page
+
+    const refBtn = document.createElement("button");
+    refBtn.textContent = "Try Again";
+    refBtn.addEventListener('click', () => window.location.reload);
+
+
+// Add an event listener to the buttons that call playRound function with the correct playerSelection when clicked
+// if ((computerScore && humanScore) < 5) {
+    rock.addEventListener('click', () => playRound('rock'));
+    paper.addEventListener('click', () => playRound('paper'));
+    scissors.addEventListener('click', () => playRound('scissors'));
+// } else {
+//     rock.addEventListener('click', () => (announcer.textContent = "The game is over, refresh to restart."));
+//     paper.addEventListener('click', () => (announcer.textContent = "The game is over, refresh to restart."));
+//     scissors.addEventListener('click', () => (announcer.textContent = "The game is over, refresh to restart."));
+// }
+
+// console.log(playGame());
