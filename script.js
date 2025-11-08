2c3204cc1e77b08dd835e5ca819f447aebba2c01
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-11-07 19:33:59 -0800

    Clean up old code

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// console.log("Hello World!")
-// const interface = document.querySelector("#interface");
-// interface.style.cssText = "color: blue; margin: auto; width; 500px;";
-
 // Write function getComputerChoice so that getComputerChoice will return "Rock", "Paper" or "Scissors"
-
 
 function getComputerChoice() {
     let compChoice = Math.floor(Math.random() * 3) + 1;
@@ -15,18 +10,6 @@ function getComputerChoice() {
         return "Scissors"
     }
 }
-
-// console.log(getComputerChoice());
-
-// Write function getHumanChoice so that the user is prompted to enter either "Rock", "Paper" or "Scissors", and the function returns the choice
-// Make getHumanChoice case-insensitive
-
-// function getHumanChoice() {
-//     let humChoice = prompt("Rock, Paper, or Scissors?", "Rock");
-//     humChoice = humChoice.toLowerCase();
-//     return humChoice;
-// }
-// console.log(getHumanChoice());
 
 // Declare the players' score variables with an initial value of 0
 
@@ -69,74 +52,39 @@ function playRound(humanSelection) {
         }
         scoreDiv.textContent = `You: ${humanScore} Computer: ${computerScore}`;
     } 
-    // else if ((humanScore === 5) || (computerScore === 5)) {
-    // }
 }
+  
+// Create constants and add text to the buttons
 
-// function playGame(humanSelection) {
-//     if ((humanScore && computerScore) < 5) {
-//         playRound(humanSelection);
-//     } else if (humanScore === 5) {
-//         announcer.textContent = "You are the winner!!!";
-//     } else {
-//         announcer.textContent = "The computer wins, try again.";
-//     }
-// }
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take both choices as arguments
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissors.textContent = "Scissors";
+    
+// Create results section
 
-// let humanSelection;
-// let computerSelection;
+const scoreBoard = document.querySelector("#results");
+scoreBoard.style.cssText = "padding: 20px; text-align: center; font-weight: bold";
+scoreBoard.textContent = "Score";
 
-// for(let i = 0; i < 5; i++) {
-    //     // const humanSelection;
-    //     // playRound(humanSelection, computerSelection);
-    //     // console.log(playRound(humanSelection, computerSelection));
-    //     console.log("Human Score: " + humanScore);
-    //     console.log("Computer Score: " + computerScore);
-    // }
-    
-    
-    
-    // Create constants and add text to the buttons
-    
-    const rock = document.querySelector("#rock");
-    const paper = document.querySelector("#paper");
-    const scissors = document.querySelector("#scissors");
-    
-    rock.textContent = "Rock";
-    paper.textContent = "Paper";
-    scissors.textContent = "Scissors";
-    
-    // Create results section
-    
-    const scoreBoard = document.querySelector("#results");
-    scoreBoard.style.cssText = "padding: 20px; text-align: center; font-weight: bold";
-    scoreBoard.textContent = "Score";
-    
-    const scoreDiv = document.createElement("div");
-    scoreBoard.appendChild(scoreDiv);
-    scoreDiv.textContent = `You: ${humanScore} Computer: ${computerScore}`;
+const scoreDiv = document.createElement("div");
+scoreBoard.appendChild(scoreDiv);
+scoreDiv.textContent = `You: ${humanScore} Computer: ${computerScore}`;
 
-    const announcer = document.createElement("div");
-    scoreBoard.appendChild(announcer);
+const announcer = document.createElement("div");
+scoreBoard.appendChild(announcer);
 
-    // Create button that refreshes the page
+// Create button that refreshes the page
 
-    const refBtn = document.createElement("button");
-    refBtn.textContent = "Try Again";
-    refBtn.addEventListener('click', () => window.location.reload());
-
+const refBtn = document.createElement("button");
+refBtn.textContent = "Try Again";
+refBtn.addEventListener('click', () => window.location.reload());
 
 // Add an event listener to the buttons that call playRound function with the correct playerSelection when clicked
-// if ((computerScore && humanScore) < 5) {
-    rock.addEventListener('click', () => playRound('rock'));
-    paper.addEventListener('click', () => playRound('paper'));
-    scissors.addEventListener('click', () => playRound('scissors'));
-// } else {
-//     rock.addEventListener('click', () => (announcer.textContent = "The game is over, refresh to restart."));
-//     paper.addEventListener('click', () => (announcer.textContent = "The game is over, refresh to restart."));
-//     scissors.addEventListener('click', () => (announcer.textContent = "The game is over, refresh to restart."));
-// }
 
-// console.log(playGame());
+rock.addEventListener('click', () => playRound('rock'));
+paper.addEventListener('click', () => playRound('paper'));
+scissors.addEventListener('click', () => playRound('scissors'));
